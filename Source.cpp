a53fafb6014bef66c6eaf54f2eeb8afb24c91279
hoanghieu13ete <hoanghieu13ete@gmail.com>
2018-05-03 15:11:34 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,34 +2,40 @@
 
 int main()
 {
-	ifstream read;
-	read.open("data/bg.txt");
+	//ifstream read;
+	//read.open("trainingdata/bg.txt");
 
-	string line;
+	//string line;
 
-	//VideoCapture Cap(0);
+	VideoCapture Cap(0);
 
 	Rect result;
+	vector<Rect> list;
 	Mat frame, gray;
+	vector<Rect> possibleChars;
 
-	while (getline(read, line))
-		//while (Cap.read(frame))
+	//while (getline(read, line))
+	while (Cap.read(frame))
 	{
-
-		frame = imread("data/" + line, 1);
+		//frame = imread("trainingdata/" + line, 1);
+		resize(frame, frame, Size(480, 360));
 		cvtColor(frame, gray, CV_BGR2GRAY);
 
-		result = FindPlate(gray);
+		list = FindLicensePlate(gray);
+		result = FindBestMat(list, gray, possibleChars);
 
 		if (!result.empty())
 		{
 			rectangle(frame, result, Scalar(0, 255, 0));
-			/*cv::line(frame(result), Point(frame(result).cols / 2, 0), Point(frame(result).cols / 2, frame(result).rows), Scalar(0, 255, 0));
-			cv::line(frame(result), Point(0, frame(result).rows / 2), Point(frame(result).cols, frame(result).rows / 2), Scalar(0, 255, 0));
-			imshow("plate", frame(result));*/
 		}
+
+		for (auto rect : possibleChars)
+		{
+			rectangle(frame(result), resizeRect(result.size(), rect), Scalar(0, 0, 255));
+		}
+
 		imshow("imshow", frame);
-		waitKey(0);
+		waitKey(25);
 	}
 
 	return 0;
